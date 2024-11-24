13012198d9d8478162cfd54772c58224005122c8
Tonipenyallop <kimtesu521@gmail.com>
2024-11-24 17:56:06 +0900

    add: type package

--- main.go
+++ main.go
@@ -7,95 +7,11 @@ import (
 	"strconv"
 	"sync"
 
+	"github.com/Tonipenyallop/pokedex-api/types"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/mtslzr/pokeapi-go"
 )
-
-// Struct to represent Pokémon details from PokéAPI
-type PokemonAPIResponse struct {
-	Name   string `json:"name"`
-	Weight int    `json:"weight"`
-	Height int    `json:"height"`
-}
-
-type Pokemon struct {
-	Abilities []struct {
-		Ability struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"ability"`
-		IsHidden bool `json:"is_hidden"`
-		Slot     int  `json:"slot"`
-	} `json:"abilities"`
-	BaseExperience int `json:"base_experience"`
-	Forms          []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"forms"`
-	GameIndices []struct {
-		GameIndex int `json:"game_index"`
-		Version   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
-	} `json:"game_indices"`
-	Height                 int           `json:"height"`
-	HeldItems              []interface{} `json:"held_items"`
-	ID                     int           `json:"id"`
-	IsDefault              bool          `json:"is_default"`
-	LocationAreaEncounters string        `json:"location_area_encounters"`
-	Moves                  []struct {
-		Move struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"move"`
-		VersionGroupDetails []struct {
-			LevelLearnedAt  int `json:"level_learned_at"`
-			MoveLearnMethod struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"move_learn_method"`
-			VersionGroup struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version_group"`
-		} `json:"version_group_details"`
-	} `json:"moves"`
-	Name    string `json:"name"`
-	Order   int    `json:"order"`
-	Species struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"species"`
-	Sprites struct {
-		BackDefault      string      `json:"back_default"`
-		BackFemale       interface{} `json:"back_female"`
-		BackShiny        string      `json:"back_shiny"`
-		BackShinyFemale  interface{} `json:"back_shiny_female"`
-		FrontDefault     string      `json:"front_default"`
-		FrontFemale      interface{} `json:"front_female"`
-		FrontShiny       string      `json:"front_shiny"`
-		FrontShinyFemale interface{} `json:"front_shiny_female"`
-	} `json:"sprites"`
-	Stats []struct {
-		BaseStat int `json:"base_stat"`
-		Effort   int `json:"effort"`
-		Stat     struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"stat"`
-	} `json:"stats"`
-	Types []struct {
-		Slot int `json:"slot"`
-		Type struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"type"`
-	} `json:"types"`
-	Weight int `json:"weight"`
-}
-
 func main() {
 	// Initialize Gin router
 	router := gin.Default()
@@ -142,7 +58,7 @@ func getPokemonDetails(c *gin.Context) {
 	}
 
 	var wg sync.WaitGroup
-	pokemons := make([]Pokemon, convertedId)
+	pokemons := make([]types.Pokemon, convertedId)
 
 	for i := 0; i < convertedId; i++ {
 		wg.Add(1)
@@ -152,7 +68,7 @@ func getPokemonDetails(c *gin.Context) {
 			if err2 != nil {
 				log.Fatal("Failed to fetch pokemonDetail", err2)
 			}
-			pokemons[i] = Pokemon(pokemonDetail)
+			pokemons[i] = types.Pokemon(pokemonDetail)
 		}()
 
 	}
