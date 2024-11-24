584a7b6c8fdfdb5d0c68e6a32b646f32c37c9602
Tonipenyallop <kimtesu521@gmail.com>
2024-11-24 17:26:10 +0900

    optimize: pokemon api

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -17,6 +19,83 @@ type PokemonAPIResponse struct {
 	Height int    `json:"height"`
 }
 
+type Pokemon struct {
+	Abilities []struct {
+		Ability struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"ability"`
+		IsHidden bool `json:"is_hidden"`
+		Slot     int  `json:"slot"`
+	} `json:"abilities"`
+	BaseExperience int `json:"base_experience"`
+	Forms          []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"forms"`
+	GameIndices []struct {
+		GameIndex int `json:"game_index"`
+		Version   struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version"`
+	} `json:"game_indices"`
+	Height                 int           `json:"height"`
+	HeldItems              []interface{} `json:"held_items"`
+	ID                     int           `json:"id"`
+	IsDefault              bool          `json:"is_default"`
+	LocationAreaEncounters string        `json:"location_area_encounters"`
+	Moves                  []struct {
+		Move struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"move"`
+		VersionGroupDetails []struct {
+			LevelLearnedAt  int `json:"level_learned_at"`
+			MoveLearnMethod struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"move_learn_method"`
+			VersionGroup struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version_group"`
+		} `json:"version_group_details"`
+	} `json:"moves"`
+	Name    string `json:"name"`
+	Order   int    `json:"order"`
+	Species struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"species"`
+	Sprites struct {
+		BackDefault      string      `json:"back_default"`
+		BackFemale       interface{} `json:"back_female"`
+		BackShiny        string      `json:"back_shiny"`
+		BackShinyFemale  interface{} `json:"back_shiny_female"`
+		FrontDefault     string      `json:"front_default"`
+		FrontFemale      interface{} `json:"front_female"`
+		FrontShiny       string      `json:"front_shiny"`
+		FrontShinyFemale interface{} `json:"front_shiny_female"`
+	} `json:"sprites"`
+	Stats []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
+	Weight int `json:"weight"`
+}
+
 func main() {
 	// Initialize Gin router
 	router := gin.Default()
@@ -24,15 +103,13 @@ func main() {
 	// Cors setting
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{"http://localhost:5173"},
-		AllowMethods: []string{"GET","POST"},
-		AllowHeaders: []string{"Origin","Content-type","Authorization"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Origin", "Content-type", "Authorization"},
 	}))
-	
 
 	// Define routes
 	router.GET("/pokemon", getPokemons)
-	router.GET("/pokemon/:pokemonId", getPokemonDetail)
-
+	router.GET("/pokemon/:pokemonId", getPokemonDetails)
 
 	// Start the server
 	router.Run("localhost:8080")
@@ -42,36 +119,47 @@ func main() {
 func getPokemons(c *gin.Context) {
 	fmt.Println("BE getPokemons are called mate")
 	// URL for the PokéAPI
-	pokemons, err := pokeapi.Resource("pokemon",0,151)
+	pokemons, err := pokeapi.Resource("pokemon", 0, 151)
 
-	if (err != nil){
+	if err != nil {
 		panic("failed to fetch pokemons")
 	}
 
-
-	c.IndentedJSON(http.StatusOK,pokemons)
+	c.IndentedJSON(http.StatusOK, pokemons)
 }
 
-
-func getPokemonDetail(c *gin.Context){
+func getPokemonDetails(c *gin.Context) {
 	fmt.Println("getPokemonDetail was called")
 	pokemonId, booleanValue := c.Params.Get("pokemonId")
-	
-	if (!booleanValue){
+
+	if !booleanValue {
 		log.Fatal("Failed to get pokemonId param")
 	}
 
-	 pokemonDetail, err2 := pokeapi.Pokemon(pokemonId)
+	convertedId, err := strconv.Atoi(pokemonId)
+	if err != nil {
+		log.Fatal("Failed to convert string to int")
+	}
 
-	 if (err2 != nil){
-		log.Fatal("Failed to fetch pokemonDetail",err2)
-	 }
+	var wg sync.WaitGroup
+	pokemons := make([]Pokemon, convertedId)
 
+	for i := 0; i < convertedId; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			pokemonDetail, err2 := pokeapi.Pokemon(strconv.Itoa(i + 1))
+			if err2 != nil {
+				log.Fatal("Failed to fetch pokemonDetail", err2)
+			}
+			pokemons[i] = Pokemon(pokemonDetail)
+		}()
 
-	 c.IndentedJSON(http.StatusOK, pokemonDetail)
-	 
-	
+	}
+
+	// Need to wait
+	wg.Wait()
+
+	c.IndentedJSON(http.StatusOK, pokemons)
+
 }
-
-
-
