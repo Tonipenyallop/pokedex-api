cd8ad80124b218f9044a2b48a81e1e649362aff8
Tonipenyallop <kimtesu521@gmail.com>
2024-12-01 20:46:45 +0900

    add: all gen pokemons

--- constants/pokemon.go
+++ constants/pokemon.go
@@ -6,4 +6,8 @@ var GENERATION_MAP = map[string][2]int{
 	"third":  {252, 386},
 	"fourth": {387, 493},
 	"fifth": {494, 649},
+	"sixth": {650, 721},
+	"seventh": {722, 809},
+	"eighth": {810, 905},
+	"ninth": {906, 1025},
 }
