189f7647b7ae1203ac245bfce271439275431dcb
Tonipenyallop <kimtesu521@gmail.com>
2024-12-04 16:22:30 +0900

    clean up detail method

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
-	"sync"
 
 	pokemonService "github.com/Tonipenyallop/pokedex-api/services"
 
@@ -46,8 +44,6 @@ func getAllPokemons(c *gin.Context) {
 }
 
 func getPokemonDetails(c *gin.Context) {
-	fmt.Println("getPokemonDetail was called")
-
 	// Get pokemonId from URL parameter
 	pokemonId, found := c.Params.Get("pokemonId")
 	if !found {
@@ -55,10 +51,7 @@ func getPokemonDetails(c *gin.Context) {
 		return
 	}
 
-	isRange := c.Query("isRange")
 
-	// If single data request
-	if isRange == "true" {
 		formattedUrl := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", pokemonId)
 
 		resp, err := http.Get(formattedUrl)
@@ -82,62 +75,8 @@ func getPokemonDetails(c *gin.Context) {
 		}
 
 		// Return single Pokemon as an array for consistency
-		c.IndentedJSON(http.StatusOK, []types.Pokemon{pokemon})
-		return
-	}
-
-	// Handle range data
-	convertedId, err := strconv.Atoi(pokemonId)
-	if err != nil || convertedId < 1 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid pokemonId"})
-		return
-	}
-
-	var wg sync.WaitGroup
-
-	// Pre-allocate a slice for ordered insertion
-	pokemons := make([]types.Pokemon, convertedId)
-
-	// Rate-limiting channel
-	rateLimiter := make(chan struct{}, 10) // Allow up to 10 concurrent requests
-
-	for i := 1; i <= convertedId; i++ {
-		wg.Add(1)
-		rateLimiter <- struct{}{} // Acquire a slot
-
-		go func(index int) {
-			defer wg.Done()
-			defer func() { <-rateLimiter }() // Release the slot
-
-			formattedUrl := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", index)
-			resp, err := http.Get(formattedUrl)
-			if err != nil {
-				fmt.Printf("Failed to fetch Pokemon details for ID %d: %v\n", index, err)
-				return
-			}
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("Failed to read response body for ID %d: %v\n", index, err)
-				return
-			}
-
-			var pokemon types.Pokemon
-			err = json.Unmarshal(body, &pokemon)
-			if err != nil {
-				fmt.Printf("Failed to decode JSON for ID %d: %v\n", index, err)
-				return
-			}
-
-			// Insert Pokemon into the correct index
-			pokemons[index-1] = pokemon
-		}(i)
-	}
-
-	wg.Wait()
-
-	c.IndentedJSON(http.StatusOK, pokemons)
+		c.IndentedJSON(http.StatusOK, pokemon)
+	
 }
 
 func getPokemonsByGen(c *gin.Context) {
@@ -159,3 +98,5 @@ func getPokemonsByGen(c *gin.Context) {
 
 	c.IndentedJSON(http.StatusOK, pokemons)
 }
+
+
